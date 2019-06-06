45ed575793f2c523aabe92ac575c7a9044821ee3
Jitu Surve <jitusurve@gmail.com>
2019-06-06 23:41:04 +0530

    SIP done.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,12 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  Amount:number;
+  Rate:number;
+  Period:number;
+  Total:string;
 
   constructor() {}
 
   CalculateSIPReturn()
   {
-    
+    let InterestForAnnualFrequency:number;
+    InterestForAnnualFrequency = (this.Rate/100)/12;
+    this.Total = (this.Amount * (Math.pow((1+ InterestForAnnualFrequency),this.Period)-1)/InterestForAnnualFrequency*(1+InterestForAnnualFrequency)).toFixed(2);
   }
 
 }
